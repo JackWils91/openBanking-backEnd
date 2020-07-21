4fa9925d1b11b8a02544297a01fe7d903e560074
JackWils91 <jackdavidwilson@gmail.com>
2020-07-21 13:52:48 +0100

    cors policy added

--- index.js
+++ index.js
@@ -13,7 +13,18 @@ const pusher = new Pusher({
   encrypted: true,
 });
 
-app.use(cors());
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
